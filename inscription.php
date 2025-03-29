f0920a1adbebc380b2af2fb9e553e7bfcbc5b66a
shaymkh <shaymamokhtari@gmail.com>
2025-03-29 02:32:16 +0100

    Update inscription.php

--- inscription.php
+++ inscription.php
@@ -42,7 +42,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                     'lastName' => "$lastName",
                     'firstName' => "$firstName",
                     'phone' => "$phone",
-                    'email' => htmlspecialchars($email),
+                    'email' => $email,
                     'dob' => $dob,
                   
                 ];
