a799077950acc4b3c71add917066131dbfb1d3a5
shaymkh <shaymamokhtari@gmail.com>
2025-05-11 23:59:31 +0200

    Update accueil.js

--- java/accueil.js
+++ java/accueil.js
@@ -1,4 +1,5 @@
-/// Fonction de basculement entre le thème clair et sombre, chagement du css du a cette regle
+// Fonction de basculement entre le thème clair et sombre, chagement du css du a cette regle
+// le fichier devrait plutot s'appeler interface.js 
 function toggleTheme() {
   const currentTheme = document.documentElement.getAttribute("data-theme");
   const newTheme = currentTheme === "dark" ? "light" : "dark";
