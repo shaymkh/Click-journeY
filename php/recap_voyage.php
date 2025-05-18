03d9976938f82a8b4e4fc9540dfd4de71a6e1d9f
shaymkh <shaymamokhtari@gmail.com>
2025-05-18 22:46:27 +0200

    Update recap_voyage.php

--- php/recap_voyage.php
+++ php/recap_voyage.php
@@ -71,6 +71,7 @@ $nbEtapes = count($details['etapes']);
   <link rel="stylesheet" href="detail.css">
   <link rel="stylesheet" href="recap_voyage.css">
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap" rel="stylesheet">
+     <link id="theme-css" rel="stylesheet" href="clair.css">
 </head>
 <body<?= $readonly ? ' class="readonly"' : '' ?>>
 
