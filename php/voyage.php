238f417b50ac44b2c78de194fa56a5f2f7938415
mondir10 <mondirboughrous10@gmail.com>
2025-05-19 04:10:59 +0200

    Update voyage.php

--- php/voyage.php
+++ php/voyage.php
@@ -19,7 +19,6 @@ if (!is_array($voyages)) $voyages = [];
     <div class="logo">CY City Adventure</div>
     <ul class="interface-links">
       <li><a href="homepage.html">Accueil</a></li>
-      <li><a href="presentationn.html">Présentation</a></li>
       <li><a href="inscription.php">S'inscrire</a></li>
       <li><a href="login.php">Se connecter</a></li>
       <li><a href="profil.php">Profil</a></li>
